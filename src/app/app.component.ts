298349613b82d64fd8d1062f13cf688d339aa568
kpribbeck <kpribbeck@gmail.com>
2026-04-15 11:43:53 -0400

    fix component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   serverData: any;
 
+  constructor() { }
+
   ngOnInit() {
-    // Simulating an API call from your CQRS Backend
     this.serverData = {
       layoutType: "VERTICAL_SPLIT_WITH_TICKER",
       components: {
         mainContent: {
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Angular 8 Simulation active.",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. This is your TV Engine test.",
           imageUrl: "https://picsum.photos/1080/1920"
         },
         footer: {
-          msg: "+++ LEGACY ANGULAR ENGINE RUNNING +++ VERSION 8.0.0 +++ SYSTEM STABLE +++",
+          msg: "+++ SYSTEM STATUS: ONLINE +++ DEPLOYMENT TEST +++",
           speed: "15s"
         }
       }
