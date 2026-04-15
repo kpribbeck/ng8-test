c52a18d3e77eaffd92e5f7577120495c33c44d43
kpribbeck <kpribbeck@gmail.com>
2026-04-15 11:03:33 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng8-test';
+export class AppComponent implements OnInit {
+  serverData: any;
+
+  ngOnInit() {
+    // Simulating an API call from your CQRS Backend
+    this.serverData = {
+      layoutType: "VERTICAL_SPLIT_WITH_TICKER",
+      components: {
+        mainContent: {
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Angular 8 Simulation active.",
+          imageUrl: "https://picsum.photos/1080/1920"
+        },
+        footer: {
+          msg: "+++ LEGACY ANGULAR ENGINE RUNNING +++ VERSION 8.0.0 +++ SYSTEM STABLE +++",
+          speed: "15s"
+        }
+      }
+    };
+  }
 }
